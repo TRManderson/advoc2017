edffb8c3979d5348a82b689f859e9aaaa7dce1f1
Tom Manderson <me@trm.io>
2017-12-03 13:10:02 +1000

    add day 1 A solution

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,5 +1,14 @@
 module Main where
 
+solnA :: String -> Int
+solnA s = snd $ foldr step (head conv, 0) conv
+  where
+    conv = map (read . (:[])) s
+    step n (l, acc) = case l == n of
+      True -> (l, acc + n)
+      False -> (n, acc)
+
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  print . solnA =<< getLine
