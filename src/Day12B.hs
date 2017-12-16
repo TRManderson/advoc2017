67136f63e54d05a8b5908562a1145169dad4f2ab
Tom Manderson <me@trm.io>
2017-12-16 13:16:02 +1000

    Add day 12

---
+++ src/Day12B.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+import Data.Graph.Inductive.Graph (mkUGraph)
+import Data.Graph.Inductive.Basic (undir)
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Graph.Inductive.Query.DFS (scc)
+import Data.Attoparsec.Text
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+
+buildGraph :: [(Int, [Int])] -> Gr () ()
+buildGraph ns = undir $ mkUGraph nodes edges
+  where
+    nodes = fst <$> ns
+    edges = do
+      (nodeA, nodesB) <- ns
+      nodeB <- nodesB
+      return (nodeA, nodeB)
+
+
+pLine = (,) <$> (decimal <* string " <-> ") <*> sepBy1 decimal (string ", ")
+p :: Parser [(Int, [Int])]
+p = many1' (pLine <* endOfLine)
+
+main = do
+  c <- T.getContents
+  case parseOnly p c of
+    Left v -> error v
+    Right v -> print . length . scc . buildGraph $ v
